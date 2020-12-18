ab6a3f6c636cb8ff59ebea5a51e70e0496ec7dbc
ZohaibMunir <zohaibmunir99@gmail.com>
2020-12-18 17:39:27 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
+let path = require('path');
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+console.log('stream: ', stream);
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+
+
+io.of('/stream').on('connection', stream);
+
+server.listen(3000, ()=>{
+    console.log('Server Starts listening on port 3000');
+});
---
+++ ws/stream.js
@@ -0,0 +1,41 @@
+const stream = (socket)=>{
+    socket.on('subscribe', (data)=>{
+        console.log('data: ', data);
+        //subscribe/join a room
+        socket.join(data.room);
+        socket.join(data.socketId);
+        console.log('data.room : ',data.room);
+        console.log('data.socketId : ',data.socketId);
+        console.log('socekt.rooms : ', socket.rooms);
+        
+        console.log('in the stream function');
+        //Inform other members in the room of new user's arrival
+        if(socket.adapter.rooms[data.room].length > 1){
+            socket.to(data.room).emit('new user', {socketId:data.socketId});
+        }
+
+        console.log(socket.rooms);
+    });
+
+
+    socket.on('newUserStart', (data)=>{
+        socket.to(data.to).emit('newUserStart', {sender:data.sender});
+    });
+
+
+    socket.on('sdp', (data)=>{
+        socket.to(data.to).emit('sdp', {description: data.description, sender:data.sender});
+    });
+
+
+    socket.on('ice candidates', (data)=>{
+        socket.to(data.to).emit('ice candidates', {candidate:data.candidate, sender:data.sender});
+    });
+
+
+    socket.on('chat', (data)=>{
+        socket.to(data.room).emit('chat', {sender: data.sender, msg: data.msg});
+    });
+}
+
+module.exports = stream;
